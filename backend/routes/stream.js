73ecebacd6ec4d495e79a914cfedf1f5552e2013
Flavien <saintsaens@users.noreply.github.com>
2024-12-22 15:10:39 +0100

    Change to ever ongoing stream

--- backend/routes/stream.js
+++ backend/routes/stream.js
@@ -4,7 +4,7 @@ import { fetchTracks, getStreamAtCurrentPosition } from '../services/tracks.js';
 export const streamRouter = Router();
 
 const tracks = await fetchTracks();
-const startTime = Date.now();
+const startTime = new Date('2024-05-04T13:37:00Z').getTime();
 
 streamRouter.get('/', (req, res) => {
     const stream = getStreamAtCurrentPosition(tracks, startTime);
