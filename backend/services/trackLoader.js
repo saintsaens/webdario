661a25f5db9f7f13d3806a01fbc5777f32f02b27
Flavien <saintsaens@users.noreply.github.com>
2024-12-30 13:24:33 +0100

    Create a playlist file

--- backend/services/trackLoader.js
+++ backend/services/trackLoader.js
@@ -1,19 +1,27 @@
 import fs from 'fs';
 import path from 'path';
 import { getTrackDuration } from './ffmpegService.js';
-import { AMBIENT_OST_DIR } from './paths.js';
-
-const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-};
+import { PLAYLIST_DIR, TRACKS_DIR } from './paths.js';
 
 export const fetchTracks = async () => {
-    const files = fs.readdirSync(AMBIENT_OST_DIR).filter(file => file !== '.DS_Store');
-    const paths = files.map(file => path.join(AMBIENT_OST_DIR, file));
+    const files = fs.readdirSync(TRACKS_DIR).filter(file => file !== '.DS_Store');
+    const paths = files.map(file => path.join(TRACKS_DIR, file));
     const durations = await Promise.all(paths.map(getTrackDuration));
-    return paths.map((path, i) => ({ path, duration: durations[i] }));
+    const totalDuration = durations.reduce((sum, duration) => sum + duration, 0);
+    const tracks = paths.map((path, i) => ({ path, duration: durations[i] }));
+    return { tracks, totalDuration };
+};
+
+export const createTrackListFile = async (outputFile = 'playlist.txt') => {
+    const files = fs.readdirSync(TRACKS_DIR).filter(file => file !== '.DS_Store');
+    const paths = files.map(file => path.join(TRACKS_DIR, file));
+
+    // Create content for the playlist.txt in ffmpeg's required format
+    const fileListContent = paths.map(trackPath => `file '${trackPath}'`).join('\n');
+
+    // Write the content to the output file
+    const filePath = path.join(PLAYLIST_DIR, outputFile);
+    fs.writeFileSync(filePath, fileListContent, 'utf8');
+
+    console.log(`Track list file created: ${filePath}`);
 };
