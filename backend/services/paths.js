a6ff5ba34ac98943916139c7cc0f39ac7861d1c5
Flavien <saintsaens@users.noreply.github.com>
2024-12-30 13:23:34 +0100

    Add more paths

--- backend/services/paths.js
+++ backend/services/paths.js
@@ -3,5 +3,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const __backendDir = path.join(__dirname, '../');
 
-export const AMBIENT_OST_DIR = path.join(__dirname, "../assets/music/Ambient OST");
+export const TRACKS_DIR = path.join(__backendDir, "../../lofi");
+export const PLAYLIST_DIR = path.join(__backendDir, "./assets/music");
