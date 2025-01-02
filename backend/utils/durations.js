a52ac7c18046d9b46ca26de5e26dd7ae4d72583d
Flavien <saintsaens@users.noreply.github.com>
2025-01-02 14:55:35 +0100

    Add utils

---
+++ backend/utils/durations.js
@@ -0,0 +1,10 @@
+import { fetchTracks } from "../services/trackLoader.js";
+
+export const generateStartTime = async () => {
+    const startTime = new Date('2024-05-04T13:37:00Z').getTime();
+    const now = Date.now();
+    const { totalDuration } = await fetchTracks();
+    const elapsed = (now - startTime) % totalDuration;
+
+    return elapsed;
+};
