97bc65812ef77f5c7a39c08f40ee883df9a80231
Flavien <saintsaens@users.noreply.github.com>
2025-01-03 21:08:07 +0100

    Add an about link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import TrackMetadata from './components/TrackMetadata';
 import Stream from './components/Stream';
 import Clock from './components/Clock';
 import './styles/style.css';
+import About from "./components/About";
 
 function App() {
     return (
         <>
             <Stream />
+            <About />
         </>
     );
 }
---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <a href="https://github.com/saintsaens/webdario" className="about-link" target="_blank" rel="noopener noreferrer">
+      About
+    </a>
+  );
+};
+
+export default About;
