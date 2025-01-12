677077c714853ab0e19ba440dc1bb2eb2691850a
Flavien <saintsaens@users.noreply.github.com>
2025-01-12 18:21:45 +0100

    Add credentials for miniO

--- backend/db/index.js
+++ backend/db/index.js
@@ -5,6 +5,8 @@ dotenv.config();
 
 export const minioClient = new Minio.Client({
     endPoint: process.env.MINIO_ENDPOINT,
+    accessKey: process.env.MINIO_ACCESS_KEY,
+    secretKey: process.env.MINIO_SECRET_KEY
 });
 
 export const getBucketByChannel = (channel) => {
