2fea22d7c3b0e1d11ae539fc042878a4345cd851
Flavien <saintsaens@users.noreply.github.com>
2025-01-20 14:11:45 +0100

    Update About link

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
   return (
-    <a href="https://github.com/saintsaens/webdario" target="_blank" rel="noopener noreferrer" className="about-button">
+    <a href="https://flavienrobert.notion.site/About-Coudradio-18086814da2780f8aa1bd29d6f9ac6b2" target="_blank" rel="noopener noreferrer" className="about-button">
       About →
     </a>
   );
