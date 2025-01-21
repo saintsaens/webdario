f24dab15f127fef429b590ed1278a07d1d4a6162
Flavien <saintsaens@users.noreply.github.com>
2025-01-21 14:08:24 +0100

    Create coudrier radio

--- backend/services/radioService.js
+++ backend/services/radioService.js
@@ -3,12 +3,16 @@ import { getTracklist } from "./tracklistService.js";
 
 export const createRadio = async () => {
     console.log("Creating radio…");
-    console.log("Creating lofi channel…");
-    const lofiChannel = await createChannel(process.env.LOFI_CHANNEL_NAME);
-    console.log(`Lofi MPD available: ${lofiChannel}`);
+    // console.log("Creating lofi channel…");
+    // const lofiChannel = await createChannel(process.env.LOFI_CHANNEL_NAME);
+    // console.log(`Lofi MPD available: ${lofiChannel}`);
+    console.log("Creating coudrier channel…");
+    const coudrierChannel = await createChannel(process.env.COUDRIER_CHANNEL_NAME);
+    console.log(`Coudrier MPD available: ${coudrierChannel}`);
 
     const radio = {
-        lofi: lofiChannel,
+        // lofi: lofiChannel,
+        coudrier: coudrierChannel
     };
 
     return radio;
@@ -17,6 +21,7 @@ export const createRadio = async () => {
 export const createChannel = async (channelName) => {
     console.log("Fetching tracklist…");
     const tracklist = await getTracklist(channelName);
+    console.log(`Found ${tracklist.length} tracks`);
     
     const channel = await createUnifiedMPD(tracklist, channelName);
 
