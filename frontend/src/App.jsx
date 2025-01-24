11a5028be0b53ba824e4d125429a0b5985820de2
Flavien <saintsaens@users.noreply.github.com>
2025-01-24 23:17:32 +0100

    Test allegiances

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,13 @@ import './styles/style.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from "./components/AppRoutes";
 import About from "./components/About";
+import Avatar from "./components/Avatar";
+import Allegiance from "./components/Allegiance";
 
 const App = () => {
     return (
         <>
+            <Allegiance />
             <Router>
                 <AppRoutes />
             </Router>
---
+++ frontend/src/components/Avatar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Koroso from "../styles/allegiances/koroso-300.png";
+
+const Avatar = () => {
+    return (
+        <>
+            <img src={Koroso} alt="Top Right" className="top-right-image" />
+        </>
+    );
+}
+
+export default Avatar;
