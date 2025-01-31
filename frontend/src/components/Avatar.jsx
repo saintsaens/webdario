c56349483d498bbc7add794e6430e9c6d60165ab
Flavien <saintsaens@users.noreply.github.com>
2025-01-31 17:58:00 +0100

    Continue migrating to Joy UI

--- frontend/src/components/Avatar.jsx
+++ frontend/src/components/Avatar.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import Butler from "../styles/allegiances/butler-300.png";
+import Box from '@mui/joy/Box';
 
 const Avatar = () => {
     return (
-        <>
-            <Link to="/welcome">
-                <img src={Butler} alt="Top Right" className="avatar" />
-            </Link>
-        </>
+        <Link to="/welcome">
+            <Box
+                component="img"
+                src={Butler}
+                sx={{
+                    height: '50px',
+                    opacity: 0.8,
+                    display: 'block',
+                    borderRadius: '4px',
+                    transition: 'background-color 0.15s, transform 0.03s',
+                    cursor: 'pointer',
+                    padding: '4px 8px',
+                    '&:hover': {
+                        opacity: 0.7,
+                    },
+                    '&:active': {
+                        transform: 'scale(0.95)',
+                    }
+                }}
+            />
+        </Link>
     );
-}
+};
 
 export default Avatar;
