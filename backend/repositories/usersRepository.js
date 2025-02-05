032e5d7cd2dbbb15c903774b8a84410ed9e6bede
Flavien <saintsaens@users.noreply.github.com>
2025-02-05 08:15:53 +0100

    Create basic user repo

---
+++ backend/repositories/usersRepository.js
@@ -0,0 +1,31 @@
+import db from '../db-users/index.js';
+
+export const createUser = async (username, hashedPw, role = "user") => {
+    const query = `
+        INSERT INTO users (username, hashed_pw, role)
+        VALUES ($1, $2, $3)
+        RETURNING id, username;
+    `;
+    const { rows } = await db.query(query, [username, hashedPw, role]);
+    return rows[0];
+};
+
+export const getUserById = async (id) => {
+    return query('SELECT id, username, role FROM users WHERE id = $1;', [id]);
+};
+
+export const updateUser = async (id, username, hashedPw) => {
+    const queryStr = `
+      UPDATE users
+      SET 
+        username = COALESCE($1, username),
+        hashed_pw = COALESCE($2, hashed_pw)
+      WHERE id = $3
+      RETURNING *;
+    `;
+    return query(queryStr, [username || null, hashedPw || null, id]);
+};
+
+export const deleteUser = async (id) => {
+    return query('DELETE FROM users WHERE id = $1 RETURNING *;', [id]);
+};
