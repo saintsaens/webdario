420f2e2e1179748a0114d8bb37f67f31013676df
Flavien <saintsaens@users.noreply.github.com>
2025-02-07 23:23:43 +0100

    Connect with Google

---
+++ backend/controllers/authController.js
@@ -0,0 +1,27 @@
+import passport from "passport";
+
+export const login = passport.authenticate('google');
+
+export const redirectBack = 
+    passport.authenticate("google", {
+        successRedirect: 'http://localhost:5173/',
+        failureRedirect: 'http://localhost:5173/'
+    });
+
+export const logout = async (req, res) => {
+    req.logout((err) => {
+        if (err) return res.status(500).json({error: "Logout failed"});
+        res.json({ message: 'Logout successful' });
+    });
+};
+
+export const getUserProfile = (req, res) => {
+    if (req.isAuthenticated()) {
+        res.json({
+            id: req.user.id,
+            username: req.user.username,
+        });
+    } else {
+        res.status(204).json({err: "Nope."});
+    }
+};
---
+++ backend/loaders/passportLoader.js
@@ -0,0 +1,59 @@
+import GoogleStrategy from "passport-google-oauth20";
+import db from "../db-users/index.js";
+import passport from "passport";
+import { createGoogleCredential, getGoogleCredential } from "../services/fedCredService.js";
+import { getUserById } from "../services/usersService.js";
+
+const passportLoader = (app) => {
+    app.use(passport.initialize());
+    app.use(passport.session());
+    
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:3001/api/auth/oauth2/redirect/google",
+        scope: ["profile", "email"]
+    }, async function verify(accessToken, refreshToken, profile, cb) {  
+        try {
+            if (!profile || !profile.id) {
+                throw new Error("Google profile is undefined or missing ID");
+            }
+    
+            // Check if federated credentials exist
+            const credentials = await getGoogleCredential(profile)
+            
+            if (!credentials) {
+                // User does not exist, create one
+                const {id, username, subject, provider} = await createGoogleCredential(profile);
+                return cb(null, { id, username, subject, provider });
+            } else {
+                // User exists, fetch their info
+                const userId = credentials.user_id
+                const existingUser = await getUserById(userId);
+
+                if (!existingUser) {
+                    console.log("User not found in users table.");
+                    return cb(null, false);
+                }
+                return cb(null, { id: existingUser.id, username: existingUser.username });
+            }
+        } catch (err) {
+            console.error("Error in Google authentication:", err);
+            return cb(err);
+        }
+    }));
+
+    passport.serializeUser((user, cb) => {
+        process.nextTick(() => {
+            cb(null, { id: user.id, username: user.username });
+        });
+    });
+
+    passport.deserializeUser((user, cb) => {
+        process.nextTick(() => {
+            return cb(null, user);
+        });
+    });
+};
+
+export default passportLoader;
