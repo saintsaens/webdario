b559f6af1cbd429b7705c7b2b8930097216d4c32
Flavien <saintsaens@users.noreply.github.com>
2025-02-08 13:34:36 +0100

    Use env variables

--- backend/loaders/passportLoader.js
+++ backend/loaders/passportLoader.js
@@ -11,7 +11,7 @@ const passportLoader = (app) => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3001/api/auth/oauth2/redirect/google",
+        callbackURL: `${process.env.BACKEND_URL}/auth/oauth2/redirect/google`,
         scope: ["profile", "email"]
     }, async function verify(accessToken, refreshToken, profile, cb) {  
         try {
