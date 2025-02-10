d756652616cf3a203d173d8d5ed6cfa7a9eb0bcb
Flavien <saintsaens@users.noreply.github.com>
2025-02-10 12:36:59 +0100

    Add db columns in user repo for handling time spent

--- backend/repositories/usersRepository.js
+++ backend/repositories/usersRepository.js
@@ -4,9 +4,9 @@ const tableName = `users`;
 
 export const createUser = async (username, hashedPw, role) => {
     const query = `
-        INSERT INTO ${tableName} (username, hashed_pw, role)
-        VALUES ($1, $2, $3)
-        RETURNING id, username, role;
+        INSERT INTO ${tableName} (username, hashed_pw, role, session_start_time, last_activity_time, time_spent)
+        VALUES ($1, $2, $3, NOW(), NOW(), INTERVAL '0')
+        RETURNING id, username, role, session_start_time, last_activity_time, time_spent;
     `;
     const { rows } = await db.query(query, [username, hashedPw, role]);
     return rows[0];
@@ -14,7 +14,7 @@ export const createUser = async (username, hashedPw, role) => {
 
 export const getUserById = async (id) => {
     const query = `
-        SELECT id, username, role
+        SELECT id, username, role, session_start_time, last_activity_time, time_spent
         FROM ${tableName}
         WHERE id = $1;
     `;
@@ -22,16 +22,19 @@ export const getUserById = async (id) => {
     return rows[0];
 };
 
-export const updateUser = async (id, username, hashedPw) => {
+export const updateUser = async (id, username, hashedPw, sessionStartTime, lastActivityTime, timeSpent) => {
     const query = `
         UPDATE ${tableName}
         SET 
             username = COALESCE($1, username),
-            hashed_pw = COALESCE($2, hashed_pw)
-        WHERE id = $3
-        RETURNING id, username, role;
+            hashed_pw = COALESCE($2, hashed_pw),
+            session_start_time = COALESCE($3, session_start_time),
+            last_activity_time = COALESCE($4, last_activity_time),
+            time_spent = COALESCE($5, time_spent)
+        WHERE id = $6
+        RETURNING id, username, role, session_start_time, last_activity_time, time_spent;
     `;
-    const { rows } = await db.query(query, [username || null, hashedPw || null, id]);
+    const { rows } = await db.query(query, [username || null, hashedPw || null, sessionStartTime || null, lastActivityTime || null, timeSpent || null, id]);
     return rows[0];
 };
 
@@ -39,7 +42,7 @@ export const deleteUser = async (id) => {
     const query = `
         DELETE FROM ${tableName}
         WHERE id = $1
-        RETURNING id, username, role;
+        RETURNING id, username, role, session_start_time, last_activity_time, time_spent;
     `;
     const { rows } = await db.query(query, [id]);
     return rows[0];
