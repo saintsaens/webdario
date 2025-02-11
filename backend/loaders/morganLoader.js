f30cdb3e2c101ca2ad024320eeaea0f5fb7fe093
Flavien <saintsaens@users.noreply.github.com>
2025-02-11 16:22:43 +0100

    Hide options calls

--- backend/loaders/morganLoader.js
+++ backend/loaders/morganLoader.js
@@ -1,7 +1,13 @@
 import morgan from "morgan";
 
 const morganLoader = (app) => {
-    app.use(morgan(':method :url :status [:date]'));
+    app.use(
+        morgan(
+            ':method :url :status [:date]',
+            {
+                skip: (req) => req.method === "OPTIONS",
+            }
+        ));
 };
 
 export default morganLoader;
