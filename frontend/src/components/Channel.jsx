46d49b7ae92e5023ffc9a1353b63710defc897d0
Flavien <saintsaens@users.noreply.github.com>
2025-02-11 16:24:29 +0100

    Move user logging state to the main page instead of stats

--- frontend/src/components/Channel.jsx
+++ frontend/src/components/Channel.jsx
@@ -5,16 +5,33 @@ import { useDispatch, useSelector } from "react-redux";
 import MuteToggler from "./Commands/MuteToggler";
 import Loading from "./Loading";
 import Unavailable from "./Unavailable";
+import { fetchUser, updateLastActivity, updateSessionStartTime } from "../store/features/userSlice";
 
 export default function Channel({ channelName }) {
     const audioRef = useRef(null);
     const playing = useSelector((state) => state.audioPlayer.playing);
     const error = useSelector((state) => state.audioPlayer.error);
+    const { userId } = useSelector((state) => state.user);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(setCurrentChannel(channelName));
     }, [dispatch, channelName]);
+
+    useEffect(() => {
+        dispatch(fetchUser());
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (userId) {
+            const updateActivity = () => {
+                dispatch(updateLastActivity());
+            };
+            dispatch(updateSessionStartTime());
+            const interval = setInterval(updateActivity, 59000); // Run every 59 seconds
+            return () => clearInterval(interval); // Cleanup on unmount
+        }
+    }, [dispatch, userId]);
 
     if (error) {
         return (
