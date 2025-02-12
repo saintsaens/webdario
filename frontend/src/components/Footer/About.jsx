e80cece9aa48b01c929b9cf0ecce07912c548336
Flavien <saintsaens@users.noreply.github.com>
2025-02-12 12:39:32 +0100

    Improve channel switcher

--- frontend/src/components/Footer/About.jsx
+++ frontend/src/components/Footer/About.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
 import { Typography } from "@mui/material";
+import { useSelector } from "react-redux";
 
 const aboutUrl = import.meta.env.VITE_ABOUT_URL;
 
 const About = () => {
+  const isSwitcherOpen = useSelector ((state) => state.channelSwitcher);
+
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if (event.key.toLowerCase() === "a" && !event.metaKey && !event.ctrlKey) {
+      if (event.key.toLowerCase() === "a" && !event.metaKey && !event.ctrlKey && !isSwitcherOpen) {
         window.open(aboutUrl, "_blank", "noopener,noreferrer");
       }
     };
