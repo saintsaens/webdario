f1365cb71b93e9a66e68174afbc324fa78d9f770
Flavien <saintsaens@users.noreply.github.com>
2025-02-22 21:00:40 +0100

    Hotfix: return 401 instead of 204 for unauthenticated requests

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,16 +1,17 @@
 import passport from "passport";
 
+const frontendURL = process.env.FRONTEND_URL;
+
 export const login = passport.authenticate('google');
 
-export const redirectBack = 
-    passport.authenticate("google", {
-        successRedirect: 'http://localhost:5173/',
-        failureRedirect: 'http://localhost:5173/'
-    });
+export const redirectBack = passport.authenticate("google", {
+    successRedirect: `${frontendURL}/?auth=success`,
+    failureRedirect: `${frontendURL}/?auth=failure`
+});
 
 export const logout = async (req, res) => {
     req.logout((err) => {
-        if (err) return res.status(500).json({error: "Logout failed"});
+        if (err) return res.status(500).json({ error: "Logout failed" });
         res.json({ message: 'Logout successful' });
     });
 };
@@ -25,6 +26,6 @@ export const getUserProfile = (req, res) => {
             timeSpent: req.user.timeSpent,
         });
     } else {
-        res.status(204).json({err: "Nope."});
+        res.status(401).json({ error: "User not authenticated" });
     }
 };
